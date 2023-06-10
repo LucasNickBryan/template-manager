78ac77feec9edda4982e66954a6fc53231c990bc
LucasNickBryan <rlucasnickbryan@gmail.com>
2023-06-10 22:07:33 +0300

    feat: call with api

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ While refactoring codes, structures had changed like:
 
 src/Entity        =>  database/migrations
 src/Repository    =>  app/Http/Controllers
+tests             => tests/Unit
 
-! This application supposed that we are using Laravel/Passport for authentication,
+.the equivalent of example.php is in routes/api.php, and calling test route
+
+
+!!! This application supposed that we are using Laravel/Passport for authentication,
 so installing the package is recommanded
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\TemplateManagerController;
+use App\Models\Destination;
+use App\Models\Quote;
+use App\Models\Template;
+use Faker\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +21,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('test', function () {
+    $faker = Factory::create();
+
+    $template = Template::create([
+        1,
+        'Your delivery to [quote:destination_name]',
+        "
+        Hi [user:first_name],
+        
+        Thanks for contacting us to deliver to [quote:destination_name].
+        
+        Regards,
+        
+        SAYNA team
+        "
+    ]);
+    
+    $message = (new TemplateManagerController)->getTemplateComputed(
+        $template,
+        [
+            'quote' => Quote::create([$faker->randomNumber(), $faker->randomNumber(), $faker->randomNumber(), $faker->date()])
+        ]
+    );
+    
+    echo $message->subject . "\n" . $message->content;
 });
