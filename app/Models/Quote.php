6b81baf2a916b86018f9035ce2588342ac78802e
LucasNickBryan <rlucasnickbryan@gmail.com>
2023-06-10 20:10:40 +0300

    feat: quotes migration

---
+++ app/Models/Quote.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quote extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'site_id',
+        'destination_id',
+        'date_quoted'
+    ];
+
+    public function renderHtml(Quote $quote)
+    {
+        return '<p>' . $quote->id . '</p>';
+    }
+
+    public static function renderText(Quote $quote)
+    {
+        return (string) $quote->id;
+    }
+}
---
+++ database/migrations/2023_06_10_170612_create_quotes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('site_id')->constrained('sites');
+            $table->foreignId('destination_id')->constrained('destinations');
+            $table->date('date_quoted');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
